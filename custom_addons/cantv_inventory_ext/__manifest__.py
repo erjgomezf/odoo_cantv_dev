e1d28b9870aa47eca6dcc2f5f0378196d2e470c3
ernesto gomez <requien330@gmail.com>
2025-05-28 14:25:45 -0400

    modificacion para actualizar el modulo

--- custom_addons/cantv_inventory_ext/__manifest__.py
+++ custom_addons/cantv_inventory_ext/__manifest__.py
@@ -3,13 +3,13 @@
     'summary': "Campos y funcionalidades adicionales para la gestión de inventario de fibra óptica en CANTV.",
     'version': '1.0',
     'category': 'Inventory/Inventory',
-    'author': "Tu Nombre",
+    'author': "Ernesto Gomez", # Nombre del desarrollador
     'website': "http://www.cantv.com.ve",
     'license': 'LGPL-3',
-    'depends': ['stock', 'product'], # Este módulo dependerá del módulo de inventario y producto de Odoo
+    'depends': ['stock', 'product'], # Esto es crucial
     'data': [
         'security/ir.model.access.csv',
-        'views/product_template_views.xml', # Archivo donde definiremos los nuevos campos en la vista
+        'views/product_template_views.xml',
     ],
     'installable': True,
     'application': False,
